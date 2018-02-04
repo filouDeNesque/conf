ed0c22ed287f8afa6182c9019fa959d029a709c4
filouDeNesque <p.cuquemy@yahoo.com>
2018-02-04 13:56:37 +0100

    apply new features

---
+++ page/single.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pierre
+ * Date: 04/02/2018
+ * Time: 12:01
+ */
--- public/index.php
+++ public/index.php
@@ -1,7 +1,30 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Pierre
+ * User: Paul
  * Date: 04/02/2018
  * Time: 11:28
  */
+require '../app/Autoloader.php';
+App\Autoloader::register();
+
+if(isset($_GET['p'])){
+    $p = $_GET['p'];
+}
+else{
+    $p =  'home';
+}
+
+
+//initialisation des objet
+$db = new \App\Database('blog');
+
+ob_start();
+if($p === 'home'){
+    require '../page/home.php';
+}elseif($p === 'article'){
+    require '../page/single.php';
+}
+
+$content = ob_get_clean();
+require '../page/template/default.php';
